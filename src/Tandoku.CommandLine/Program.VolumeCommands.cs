5a19d65e64be8714039c5c2f2f169fd90d542766
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2023-04-22 08:09:43 -0700

    added --moniker, --tags to tandoku volume new

--- src/Tandoku.CommandLine/Program.VolumeCommands.cs
+++ src/Tandoku.CommandLine/Program.VolumeCommands.cs
@@ -16,22 +16,27 @@ public sealed partial class Program
         var titleArgument = new Argument<string>("title", "Title of new tandoku volume");
         var pathOption = new Option<DirectoryInfo?>(new[] { "--path", "-p" }, "Containing directory for new tandoku volume")
             .LegalFilePathsOnly();
+        var monikerOption = new Option<string?>(new[] { "--moniker", "-m" }, "Optional moniker to identify volume, prepended to volume directory");
+        var tagsOption = new Option<string>(new[] { "--tags", "-t" }, "Optional comma-separated tags for volume");
         var forceOption = new Option<bool>(new[] { "--force", "-f" }, "Allow new volume in non-empty directory");
 
         var command = new Command("new", "Creates a new tandoku volume under the current or specified directory")
         {
             titleArgument,
             pathOption,
+            monikerOption,
+            tagsOption,
             forceOption,
         };
 
-        command.SetHandler(async (title, directory, force) =>
+        command.SetHandler(async (title, directory, moniker, tags, force) =>
         {
             var volumeManager = this.CreateVolumeManager();
             var path = directory?.FullName ?? this.fileSystem.Directory.GetCurrentDirectory();
-            var info = await volumeManager.CreateNewAsync(title, path, force: force);
+            var tagsArray = tags?.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            var info = await volumeManager.CreateNewAsync(title, path, moniker, tagsArray, force);
             this.console.WriteLine(@$"Created new tandoku volume ""{title}"" at {info.Path}");
-        }, titleArgument, pathOption, forceOption);
+        }, titleArgument, pathOption, monikerOption, tagsOption, forceOption);
 
         return command;
     }
--- tests/Tandoku.CommandLine.Tests/VolumeCommandTests.cs
+++ tests/Tandoku.CommandLine.Tests/VolumeCommandTests.cs
@@ -3,9 +3,18 @@
 public class VolumeCommandTests : CliTestBase
 {
     [Fact]
-    public Task New() => this.RunAndAssertAsync(
-        "volume new sample-volume/1",
-        @$"Created new tandoku volume ""sample-volume/1"" at {this.ToFullPath("sample-volume_1")}");
+    public async Task New()
+    {
+        await this.RunAndAssertAsync(
+            "volume new sample-volume/1 --moniker sv-1 --tags tag1,tag2",
+            @$"Created new tandoku volume ""sample-volume/1"" at {this.ToFullPath("sv-1-sample-volume_1")}");
+
+        this.fileSystem.GetFile(this.ToFullPath("sv-1-sample-volume_1", "volume.yaml")).TextContents.TrimEnd().Should().Be(
+@"title: sample-volume/1
+moniker: sv-1
+language: ja
+tags: [tag1, tag2]");
+    }
 
     [Fact]
     public Task NewWithPath() => this.RunAndAssertAsync(
@@ -16,4 +25,23 @@ public class VolumeCommandTests : CliTestBase
     public Task NewWithFullPath() => this.RunAndAssertAsync(
         $"volume new sample-volume/1 --path {this.ToFullPath("container")}",
         @$"Created new tandoku volume ""sample-volume/1"" at {this.ToFullPath("container", "sample-volume_1")}");
+
+    [Fact]
+    public async Task NewWithNonEmptyDirectory()
+    {
+        this.fileSystem.AddEmptyFile(this.ToFullPath("sample-volume", "existing.txt"));
+        await this.RunAndAssertAsync(
+            "volume new sample-volume",
+            expectedOutput: string.Empty,
+            expectedError: "The specified directory is not empty and force is not specified.");
+    }
+
+    [Fact]
+    public async Task NewWithNonEmptyDirectoryForce()
+    {
+        this.fileSystem.AddEmptyFile(this.ToFullPath("sample-volume", "existing.txt"));
+        await this.RunAndAssertAsync(
+            "volume new sample-volume --force",
+            @$"Created new tandoku volume ""sample-volume"" at {this.ToFullPath("sample-volume")}");
+    }
 }
