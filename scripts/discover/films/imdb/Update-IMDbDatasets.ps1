6834979becf60b49b7dd683912244da5f308ba49
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2023-06-19 11:28:36 -0700

    Update-IMDbDatasets.ps1 fix

--- scripts/discover/films/imdb/Update-IMDbDatasets.ps1
+++ scripts/discover/films/imdb/Update-IMDbDatasets.ps1
@@ -27,10 +27,12 @@ foreach ($f in $files) {
 if ($Unpack) {
     foreach ($f in $files) {
         $gzf = "$f.gz"
-        if (Test-Path $gzf) {
+        if ((Test-Path $gzf) -and ($Force -or -not (Test-Path $f))) {
             Write-Host "Unpacking $gzf"
             7z x $gzf
+            if (Test-Path $f) { Remove-Item $f }
             Move-Item data.tsv $f
+            Remove-Item $gzf
         }
     }
 }
