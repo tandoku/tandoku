775b379ee9bf46143f6973375a1eedb9af81b91e
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2023-10-10 18:05:45 -0700

    TandokuVideoImport.ps1 initial import of mp4 videos to source

--- scripts/TandokuVideoImport.ps1
+++ scripts/TandokuVideoImport.ps1
@@ -8,6 +8,19 @@ param(
     $VolumePath
 )
 
-# Collect .mp4/.mkv files at $Path (tandoku source import)
+$volume = TandokuVolumeInfo -VolumePath $VolumePath
+if (-not $volume) {
+    return
+}
+$volumePath = $volume.path
+
+# Import videos
+$videosToImport = Get-ChildItem $Path -Filter *.mp4
+if (-not $videosToImport) {
+    Write-Warning 'No videos to import found at the specified path.'
+    return
+}
+TandokuSourceImport -Path $videosToImport -VolumePath $volumePath -VersionControl binary
+
 # Find corresponding subtitles (tandoku source import)
 # Note: import reference language subtitles into /source/ref-en/ path
