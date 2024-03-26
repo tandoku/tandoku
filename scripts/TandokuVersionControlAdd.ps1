aa1161e617eaefa7515147bf41e7f4f9ce1144d8
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2024-03-25 22:48:23 -0700

    scripts/TandokuVersionControlAdd.ps1 chunk adds

--- scripts/TandokuVersionControlAdd.ps1
+++ scripts/TandokuVersionControlAdd.ps1
@@ -9,18 +9,24 @@ param(
     $Kind
 )
 
-switch ($Kind) {
-    'text' { git add $Path }
-    'binary' { dvc add $Path }
-    'auto' {
-        # TODO: check that each $Path already exists, check for $Path.dvc file to determine if binary (***for directories as well***)
-        # >>> if path doesn't exist, must assume it (was) a directory for TandokuVolumeRename to work !!
-        if ($Path.Count -eq 1 -and (Test-Path $Path -PathType Container)) {
-            # Note: this assumes any binary files under directory have already been added individually
-            # This should only be used to handle directory renames
-        } else {
-            Write-Warning "TODO: auto for multiple/non-directory paths"
+# Chunk adds to avoid errors when adding excessively large number of files
+$chunkSize = 200
+for ($i = 0; $i -lt $Path.Count; $i += $chunkSize) {
+    $items = $Path[$i..($i + $chunkSize - 1)]
+
+    switch ($Kind) {
+        'text' { git add $items }
+        'binary' { dvc add $items }
+        'auto' {
+            # TODO: check that each item already exists, check for <item>.dvc file to determine if binary (***for directories as well***)
+            # >>> if item doesn't exist, must assume it (was) a directory for TandokuVolumeRename to work !!
+            if ($items.Count -eq 1 -and (Test-Path $items -PathType Container)) {
+                # Note: this assumes any binary files under directory have already been added individually
+                # This should only be used to handle directory renames
+            } else {
+                Write-Warning "TODO: auto for multiple/non-directory paths"
+            }
+            git add $items
         }
-        git add $Path
     }
 }
