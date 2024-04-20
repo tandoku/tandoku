23a7bf0e224cb32ed2a2481e2643b9b1651cc351
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2024-04-19 22:22:37 -0700

    TandokuAnkiGenerateContent.ps1 -MaxBlocksPerFile option

--- scripts/TandokuAnkiGenerateContent.ps1
+++ scripts/TandokuAnkiGenerateContent.ps1
@@ -8,6 +8,10 @@ param(
     $VolumePath,
 
     [Parameter()]
+    [int]
+    $MaxBlocksPerFile = 500,
+
+    [Parameter()]
     [Switch]
     $UseReading
 )
@@ -33,31 +37,39 @@ if (-not (Test-Path "$volumePath/content")) {
     [void] (New-Item "$volumePath/content" -ItemType Directory)
 }
 
-$contentPath = "$volumePath/content/content.yaml"
-$cards |
-    Foreach-Object {
-        $block = @{
-            text = FormatCardText ($UseReading ? $_.nativeReading : $_.native)
-            source = @{
-                block = $_.id
-            }
-        }
-        if ($_.img -and $_.img -match "img src=`"(.+)`"") {
-            $imageName = $matches[1]
-            $block.image = @{
-                name = $imageName
-            }
-        }
-        if ($_.ref) {
-            $block.references = @{
-                en = @{
-                    text = FormatCardText $_.ref
-                }
-            }
-        }
-        (ConvertTo-Yaml $block).TrimEnd()
-        '---'
-    } |
-    Set-Content $contentPath
+$cardGroups = $cards |
+    ForEach-Object { $i = 0 } { $_; $i++ } |
+    Group-Object { [Math]::Floor($i / $MaxBlocksPerFile) + 1 }
 
-    TandokuVersionControlAdd -Path $contentPath -Kind text
+$cardGroups |
+    Foreach-Object {
+        $contentFileNum = $cardGroups.Count -gt 1 ? $_.Name : $null
+        $contentPath = "$volumePath/content/content$contentFileNum.yaml"
+        $_.Group |
+            Foreach-Object {
+                $block = @{
+                    text = FormatCardText ($UseReading ? $_.nativeReading : $_.native)
+                    source = @{
+                        block = $_.id
+                    }
+                }
+                if ($_.img -and $_.img -match "img src=`"(.+)`"") {
+                    $imageName = $matches[1]
+                    $block.image = @{
+                        name = $imageName
+                    }
+                }
+                if ($_.ref) {
+                    $block.references = @{
+                        en = @{
+                            text = FormatCardText $_.ref
+                        }
+                    }
+                }
+                (ConvertTo-Yaml $block).TrimEnd()
+                '---'
+            } |
+            Set-Content $contentPath
+
+        TandokuVersionControlAdd -Path $contentPath -Kind text
+    }
