0ca0117f18909983a87bb11b0beea966f67ae2ac
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2024-05-14 22:14:10 -0700

    TandokuAnkiGenerateContent uses TandokuCsvGenerateContent

--- scripts/TandokuAnkiGenerateContent.ps1
+++ scripts/TandokuAnkiGenerateContent.ps1
@@ -16,65 +16,54 @@ param(
     $UseReading
 )
 
-# TODO - call into TandokuCsvGenerateContent
+# TODO - generalize as needed (other Jo-Mako decks - including audio, other Anki decks)
 
-Import-Module "$PSScriptRoot/modules/tandoku-utils.psm1" -Scope Local
+$formatCardText = {
+    param($text)
 
-function FormatCardText($text) {
     $newline = [Environment]::NewLine
     $lines = $text -split '　'
     return ($lines -join "  $newline")
 }
+$extractImg = {
+    param($img)
 
-$volume = TandokuVolumeInfo -VolumePath $VolumePath
-if (-not $volume) {
-    return
-}
-$volumePath = $volume.path
-
-# TODO - generalize as needed (other Jo-Mako decks - including audio, other Anki decks)
-$cards = Get-Content $path |
-    Select-Object -Skip 3 |
-    ConvertFrom-Csv -Delimiter `t -Header @('id','native','ref','nativeReading','img')
-
-CreateDirectoryIfNotExists "$volumePath/content"
-
-$cardGroups = $cards |
-    ForEach-Object { $i = 0 } { $_; $i++ } |
-    Group-Object { [Math]::Floor($i / $MaxBlocksPerFile) + 1 }
-
-$cardGroups |
-    Foreach-Object {
-        $contentFileName = $cardGroups.Count -gt 1 ?
-            "cards$($_.Name).content.yaml" :
-            'content.yaml'
-        $contentPath = "$volumePath/content/$contentFileName"
-
-        $_.Group |
-            Foreach-Object {
-                $block = @{
-                    text = FormatCardText ($UseReading ? $_.nativeReading : $_.native)
-                    source = @{
-                        block = $_.id
-                    }
-                }
-                if ($_.img -and $_.img -match "img src=`"(.+)`"") {
-                    $imageName = $matches[1]
-                    $block.image = @{
-                        name = $imageName
-                    }
-                }
-                if ($_.ref) {
-                    $block.references = @{
-                        en = @{
-                            text = FormatCardText $_.ref
-                        }
-                    }
-                }
-                (ConvertTo-Yaml $block).TrimEnd()
-                '---'
-            } |
-            Set-Content $contentPath
-
-        TandokuVersionControlAdd -Path $contentPath -Kind text
+    if ($img -and $img -match "img src=`"(.+)`"") {
+        $imageName = $matches[1]
+        return $imageName
     }
+}
+
+$columns = @{
+    id = @{ Target = 'source.note' }
+    img = @{
+        Target = 'image.name'
+        Extractor = $extractImg
+    }
+    ref = @{
+        Target = 'references.en.text'
+        Extractor = $formatCardText
+    }
+}
+$textColumn = @{
+    Target = 'text'
+    Extractor = $formatCardText
+}
+if ($UseReading) {
+    $columns.nativeReading = $textColumn
+} else {
+    $columns.native = $textColumn
+}
+
+$csvParams = @{
+    Path = $Path
+    VolumePath = $VolumePath
+    MaxBlocksPerFile = $MaxBlocksPerFile
+    SkipLines = 3
+    Delimiter = "`t"
+    Header = @('id','native','ref','nativeReading','img')
+    BaseFileName = 'cards'
+    Columns = $columns
+}
+
+TandokuCsvGenerateContent @csvParams
