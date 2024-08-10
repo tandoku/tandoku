297e9228be170c2660bcdb3715ea96be2d9c24b7
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2024-08-09 21:26:20 -0700

    RemoveNonJapaneseTextTransform ignore commonly misrecognized chars

--- src/Tandoku.Core/Content/Transforms/RemoveNonJapaneseTextTransform.cs
+++ src/Tandoku.Core/Content/Transforms/RemoveNonJapaneseTextTransform.cs
@@ -4,9 +4,23 @@ using WanaKanaSharp;
 
 public sealed class RemoveNonJapaneseTextTransform : ContentBlockRewriter
 {
+    private static readonly IReadOnlySet<char> IgnoredKana = new HashSet<char>(['ロ']);
+    private static readonly IReadOnlySet<char> IgnoredKanji = new HashSet<char>(['口', '入', '人', '日']);
+
     public override ContentBlock? Visit(TextBlock block)
     {
-        return (string.IsNullOrWhiteSpace(block.Text) ||
-            block.Text.Any(c => WanaKana.IsKana(c) || WanaKana.IsKanji(c))) ? block : null;
+        if (string.IsNullOrWhiteSpace(block.Text))
+            return block;
+
+        var kanaCount = block.Text.Count(c => WanaKana.IsKana(c) && !IgnoredKana.Contains(c));
+        var allKanaCount = block.Text.Count(WanaKana.IsKana);
+        if (kanaCount > 1 || (kanaCount > 0 && allKanaCount > 1))
+            return block;
+
+        var kanjiCount = block.Text.Count(c => WanaKana.IsKanji(c) && !IgnoredKanji.Contains(c));
+        if (kanjiCount > 0)
+            return block;
+
+        return null;
     }
 }
