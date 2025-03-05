280b2a7e3e0e82f1af62f6f429e170e6fc91894a
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2025-03-04 21:44:59 -0800

    TandokuConfig support volume-scoped config

--- scripts/TandokuConfig.ps1
+++ scripts/TandokuConfig.ps1
@@ -1,16 +1,88 @@
+using namespace System.Management.Automation
+
 param(
     [Parameter()]
-    [ValidateSet('azure-computer-vision.apiKey','azure-computer-vision.endpoint','core.staging','epub.staging')] # TODO - get this from some central location (generate from config exported by scripts)
+    [ArgumentCompleter({
+        param($Command, $Parameter, $WordToComplete, $CommandAst, $FakeBoundParams)
+
+        $allKeyPatterns = ([ValidKeysGenerator]::new()).GetValidValues()
+        $matching = @()
+        foreach ($keyPattern in $allKeyPatterns) {
+            if ($keyPattern.EndsWith('*')) {
+                $keyPattern = $keyPattern.Substring(0, $keyPattern.Length - 1)
+            }
+            if ($keyPattern -like "$WordToComplete*") {
+                $matching += $keyPattern
+            }
+        }
+        return $matching
+    })]
+    [ValidateScript({
+        $allKeyPatterns = ([ValidKeysGenerator]::new()).GetValidValues()
+        foreach ($keyPattern in $allKeyPatterns) {
+            if ($keyPattern.EndsWith('*')) {
+                if ($_ -like $keyPattern) {
+                    return $true
+                }
+            } else {
+                if ($_ -eq $keyPattern) {
+                    return $true
+                }
+            }
+        }
+        return $matching
+    })]
     [String]
     $Key,
 
     [Parameter()]
-    $Value
+    $Value,
+
+    [Parameter()]
+    [ValidateSet('user','volume')] # TODO - library
+    [String]
+    $Scope = 'user',
+
+    [Parameter()]
+    $Volume
 )
 
-Import-Module "$PSScriptRoot/modules/tandoku-utils.psm1" -Scope Local
+class ValidKeysGenerator : IValidateSetValuesGenerator {
+    [string[]] GetValidValues() {
+        # TODO - register/collect all workflow-params and other keys (some kind of parameters
+        # exported by scripts) so that the IValidateSetValuesGenerator implementation can provide
+        # the full set of keys instead of * patterns.
+        # ArgumentCompleter and ValidateScript can then be replaced with ValidateSet.
+        # see https://vexx32.github.io/2018/11/29/Dynamic-ValidateSet/
+        return @(
+            'azure-computer-vision.apikey',
+            'azure-computer-vision.endpoint',
+            'core.staging',
+            'epub.staging',
+            'workflow-params.*'
+        )
+    }
+}
 
-$configPath = '~/.tandoku/config.yaml'
+Import-Module "$PSScriptRoot/modules/tandoku-utils.psm1" -Scope Local
+Import-Module "$PSScriptRoot/modules/tandoku-volume.psm1" -Scope Local
+
+switch ($Scope) {
+    'volume' {
+        $Volume = ResolveVolume $Volume
+        if (-not $Volume) {
+            return
+        }
+        $volumePath = $Volume.Path
+        # TODO - add MetadataDirectoryPath to volume info
+        # and probably eventually ConfigPath as well
+        $configPath = "$volumePath/.tandoku-volume/config.yaml"
+    }
+    'user' {
+        $configPath = '~/.tandoku/config.yaml'
+    }
+}
+
 $config = (Test-Path $configPath) ?
     (Get-Content $configPath | ConvertFrom-Yaml) :
     @{}
