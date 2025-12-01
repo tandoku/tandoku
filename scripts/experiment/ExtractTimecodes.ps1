7b3e2adbf7f77584494a3682b117930df4892631
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2025-12-01 12:39:20 -0800

    scripts/experiment/ExtractTimecodes.ps1 add filename to output

--- scripts/experiment/ExtractTimecodes.ps1
+++ scripts/experiment/ExtractTimecodes.ps1
@@ -7,14 +7,20 @@ param(
 )
 
 Get-ChildItem $Path -Filter *.content.yaml |
-    ForEach-Object { Import-Yaml $_ } |
     ForEach-Object {
-        [PSCustomObject]@{
-            ordinal = $_.source.ordinal
-            start = $_.source.timecodes.start
-            end = $_.source.timecodes.end
-            ref_en_ordinal = $_.references.en.source.ordinal
-            ref_en_start = $_.references.en.source.timecodes.start
-            ref_en_end = $_.references.en.source.timecodes.end
-        }
+        $filename = Split-Path -Leaf $_
+        $blocks = Import-Yaml $_
+
+        $blocks | 
+            ForEach-Object {
+                [PSCustomObject]@{
+                    filename = $filename
+                    ordinal = $_.source.ordinal
+                    start = $_.source.timecodes.start
+                    end = $_.source.timecodes.end
+                    ref_en_ordinal = $_.references.en.source.ordinal
+                    ref_en_start = $_.references.en.source.timecodes.start
+                    ref_en_end = $_.references.en.source.timecodes.end
+                }
+            }
     }
