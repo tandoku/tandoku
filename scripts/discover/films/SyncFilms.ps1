42955896ac10af0a09b9faf08e869a34529a0fe1
Aaron Meyers <13340718+aaron-meyers@users.noreply.github.com>
2026-06-05 14:13:49 -0700

    +discover/films/SyncFilms.ps1

---
+++ scripts/discover/films/SyncFilms.ps1
@@ -0,0 +1,21 @@
+[CmdletBinding()]
+param(
+    [string]$Path = '.',
+    [switch]$Force,
+    [switch]$UpdateImdbData,
+    [switch]$UpdateNativelyData
+)
+
+$dbPath = "$Path/films.yaml"
+$dbJsonPath = "$Path/films.json"
+$sources = "$Path/sources"
+
+& "$PSScriptRoot/ImportNetflixWatchlist.ps1" -Path "$sources/netflix/netflix-my-list.json" -DatabasePath $dbPath
+
+& "$PSScriptRoot/PopulateWikidata.ps1" -DatabasePath $dbPath -Force:$Force
+
+& "$PSScriptRoot/PopulateIMDb.ps1" -DatabasePath $dbPath -ImdbDataPath "$sources/imdb" -UpdateImdbData:$UpdateImdbData
+
+& "$PSScriptRoot/PopulateNatively.ps1" -DatabasePath $dbPath -NativelyDataPath "$sources/natively" -UpdateNativelyData:$UpdateNativelyData
+
+& "$PSScriptRoot/ExportFilms.ps1" -DatabasePath $dbPath -OutputPath $dbJsonPath
